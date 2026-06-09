74c7a5a78fd34bb01f736a2f8a08d808894de1a4
tranthienquang05 <tranthienquang05@gmail.com>
2026-06-09 16:53:00 +0700

    Initial Spring Boot Project

---
+++ src/main/java/com/manga/mangaproductionmanagement/MangaProductionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.manga.mangaproductionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaProductionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaProductionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/mangaproductionmanagement/MangaProductionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.mangaproductionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaProductionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
